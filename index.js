e2a593457189f915bf7ad56c67f046f773a2d603
Charlie <146101928+clochettes@users.noreply.github.com>
2023-10-24 11:24:54 +0000

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { CLient, GatewayIntentBits, discordSort, MEssageManager } = require("discord.js");
 const Discord = require("discord.js");
-const token = ("")
+const token = require("config.json")
 const bot = new Discord.Client({
     intents:[
         GatewayIntentBits.Guilds,
