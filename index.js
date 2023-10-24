9f64a2c64fe2f4fc6091de1d6beff2958f6fd470
Charlie <146101928+clochettes@users.noreply.github.com>
2023-10-24 10:12:12 +0000

    Index.js creation and update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { CLient, GatewayIntentBits, discordSort, MEssageManager } = require("discord.js");
 const Discord = require("discord.js");
-const client = new Discord.client 
-// testazaz
+const token = ("")
+const bot = new Discord.Client({
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+})
+bot.login("token")
+
+bot.on('ready', ()=> {
+    bot.user.setActivity('Sodium Development')
+})
